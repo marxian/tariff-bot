3081abd8504b4d99b2652e8ec2a557765662557e
TomHodson <thomas.c.hodson@gmail.com>
2012-09-29 13:45:55 +0100

    add response templates to config

--- config.py
+++ config.py
@@ -3,12 +3,14 @@ configobject = {
 	"lexicon" : [
 
 		{
-		"search_criteria" : [["trade", "exports"],["tarriffs", "embargoes"]] 
+		"search_criteria" : [["trade", "imports"],["tarriffs", "embargoes"]] 
 		"relevant_data" : []
+		"response_templates" : ["Import tarrifs in {country} are {value}", "There is a {value} level of import tax in {country}"]
 		}
 		{
 		"search_criteria" : [["access", "availability"], ["power", "electricity"]]
 		"relevant_data" : []
+		"response_templates" : ["{value} of people have access to power in {country}"]
 		}
 
 
