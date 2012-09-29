2d00bbae159c64d16f731f52eca35d833c7070fc
TomHodson <thomas.c.hodson@gmail.com>
2012-09-29 13:31:14 +0100

    sketched out config a bit

--- config.py
+++ config.py
@@ -1,6 +1,18 @@
 configobject = {
 	
-	"subjects" :
+	"lexicon" : [
+
+		{
+		"search_criteria" : [["trade", "exports"],["tarriffs", "embargoes"]] 
+		"relevant_data" : []
+		}
+		{
+		"search_criteria" : [["access", "availability"], ["power", "electricity"]]
+		"relevant_data" : []
+		}
+
+
+	]
 
 
 }
