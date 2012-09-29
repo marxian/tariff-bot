0a9e1a4be97a4f32b07b3888771e1516156f3ee1
Rupert Redington <rupert@neontribe.co.uk>
2012-09-29 15:46:21 +0100

    Tariff Bot can See!

--- api.py
+++ api.py
@@ -3,20 +3,41 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 import tweepy
 import os
 from secrets import *
+from config import *
+
+def t_con():
+	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+	auth.set_access_token(access_token, access_token_secret)
+	return tweepy.API(auth)
 
 class Tweet(webapp.RequestHandler):
     def get(self):
-		auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-		auth.set_access_token(access_token, access_token_secret)
-		api = tweepy.API(auth)
+		api = t_con()
 		if not os.environ['SERVER_SOFTWARE'].startswith('Development'):
 			api.update_status('Ahem. Testing. Testing. 1, 2, 3...')
 		self.response.headers['Content-Type'] = 'text/plain'
 		self.response.out.write('Could have tweeted as ' + api.me().name)
 
+class Search(webapp.RequestHandler):
+	def get(self):
+		self.response.headers['Content-Type'] = 'text/plain'
+		api = t_con()
+		for spec in configobject['lexicon']:
+			self.response.out.write('Twitter search for ' + spec['twitter_search_term'] + '\n')
+			results = api.search(spec['twitter_search_term'])
+			for result in results:
+				self.response.out.write('\t' + result.text + '\n')
+			
+
+
+
+
 application = webapp.WSGIApplication(
-                                     [('/tweet', Tweet)],
-                                     debug=True)
+	[
+		('/tweet', Tweet),
+		('/tasks/search', Search),
+	],
+	debug=True)
 
 def main():
     run_wsgi_app(application)
--- config.py
+++ config.py
@@ -4,11 +4,12 @@ configobject = {
 
 		{
 		"twitter_search_term" : "trade",
-		"search_criteria" : [["trade", "imports"],["tarriffs", "embargoes"]],
+		"search_criteria" : [["trade", "imports"],["tariffs", "embargoes"]],
 		"relevant_data" : [],
 		"response_templates" : ["Import tarrifs in {country} are {value}", "There is a {value} level of import tax in {country}"],
 		},
 		{
+		"twitter_search_term" : "electricity access",
 		"search_criteria" : [["access", "availability"], ["power", "electricity"]],
 		"relevant_data" : [],
 		"response_templates" : ["{value} of people have access to power in {country}"],
