278c76a69f578625c32b7fe7a0ad3dbd48f17289
TomHodson <thomas.c.hodson@gmail.com>
2012-09-29 15:01:47 +0100

    fixed typos in config :P

--- config.py
+++ config.py
@@ -1,16 +1,17 @@
 configobject = {
-	
+
 	"lexicon" : [
 
 		{
-		"search_criteria" : [["trade", "imports"],["tarriffs", "embargoes"]] 
-		"relevant_data" : []
-		"response_templates" : ["Import tarrifs in {country} are {value}", "There is a {value} level of import tax in {country}"]
-		}
+		"twitter_search_term" : "trade",
+		"search_criteria" : [["trade", "imports"],["tarriffs", "embargoes"]],
+		"relevant_data" : [],
+		"response_templates" : ["Import tarrifs in {country} are {value}", "There is a {value} level of import tax in {country}"],
+		},
 		{
-		"search_criteria" : [["access", "availability"], ["power", "electricity"]]
-		"relevant_data" : []
-		"response_templates" : ["{value} of people have access to power in {country}"]
+		"search_criteria" : [["access", "availability"], ["power", "electricity"]],
+		"relevant_data" : [],
+		"response_templates" : ["{value} of people have access to power in {country}"],
 		}
 
 
