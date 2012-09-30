f4489df6d4f54fa41d9dec203b7796fd0cf275a8
TomHodson <thomas.c.hodson@gmail.com>
2012-09-30 01:36:06 +0100

    added some extra mappings

--- config.py
+++ config.py
@@ -7,7 +7,20 @@ configobject = {
 			"search_criteria" : [["trade", "import"],["tariff", "embargo"]],
 			"relevant_data" : ("Tariff rate applied simple mean all products %", 'TM.TAX.MRCH.SM.AR.ZS'),
 			"response_templates" : ["Import tariffs in {country} are {value}%", "There is a {value}% level of import tax in {country}"]
+		},
+		{
+			"twitter_search_term" : "population",
+			"search_criteria" : [["population",],["total",]],
+			"relevant_data" : ("Total Population", 'SP.POP.TOTL'),
+			"response_templates" : ["The population of {country} is {value}", "There are {value} people living in {country}"]
+		},
+		{
+			"twitter_search_term" : "life",
+			"search_criteria" : [["life","lifetime","age"],["expectancy","average"]],
+			"relevant_data" : ("Life expectancy at birth", 'SP.DYN.LE00.IN'),
+			"response_templates" : ["The life expectancy of someone in {country} is around {value}", "On average, people in {country} live to {value} years old"]
 		}
+
 	],
 	"wb_indicator_url": "http://api.worldbank.org/countries/{country}/indicators/{indicator}?per_page=10&date=2006:2015&format=json",
 	"vis_url": "http://dev.wetoffice.com/form.html?country={country}&indicator={indicator}"
